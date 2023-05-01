c4fb82c8b9741e734f6049151392ecc2fbb9db2b
Valtairnascimento <valtairnjr@gmail.com>
2023-05-01 17:13:03 -0300

    learn: use the hook useMemo in the react App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import P from 'prop-types';
-import React, { useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import './App.css';
+import P from 'prop-types';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  return <button onClick={() => incrementButton(2)}>+</button>;
-});
-Button.propTypes = {
-  incrementButton: P.func,
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [value, setValue] = useState('');
+  const [posts, setPosts] = useState([]);
+  console.log('Pai renderizou');
 
-  const incrementCounter = (num) => {
-    setCounter(counter + num);
-  };
-
+  //component did mount
+  useEffect(() => {
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
+  }, []);
   return (
     <div className="App">
-      <h1>Contar: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      <p>
+        <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+      </p>
+      {useMemo(() => {
+        return posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} />);
+      }, [posts])}
+
+      {posts.length <= 0 && <p>Não existem posts</p>}
     </div>
   );
 }
